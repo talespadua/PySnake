7b5484a15853694eb195fececf3787f5c62adb5b
talespadua <talescpadua@gmail.com>
2015-05-11 14:43:54 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+__author__ = 'tales.cpadua'
+import pygame
+
+def main():
+    pygame.init()
+    game_display = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("Snake")
+    pygame.display.update()
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main()
